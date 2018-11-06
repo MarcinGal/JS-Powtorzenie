d75690469aee385664527cae5a681fe5d4bd43a5
marcingal <marcin921125@gmail.com>
2018-11-06 11:44:16 +0100

    3exercis

--- script1.js
+++ script1.js
@@ -20,7 +20,9 @@ var log10TimesPrim = log10Times
 log10Times = null // always place const with function!
 
 log10TimesPrim()
-*/
+
+
+
 
 const tenNumbers = function numbers (i) {
     i = (i === undefined) ? 10 : i
@@ -31,3 +33,39 @@ const tenNumbers = function numbers (i) {
 }
 
 tenNumbers()
+
+
+
+
+pure vs non-pure function?
+
+
+
+function callWith5and3(func) {
+    return func(5, 3)
+}
+
+
+
+function call10Times(func) {
+
+    for (i = 0; i < 10; i++) { func('BU!') }
+}
+
+call10Times(console.log)
+
+
+numbers = ['placek', 22, 'gleba', 44, 'bezszczel']
+
+numbers.forEach((element) => console.log(element))
+*/
+
+function myOwnForEach(arr, func, thisArg) {
+    for (let i = 0; i < arr.length; i++) {
+        func.call(thisArg, arr[i], i, arr);
+    }
+}
+
+const bla = ['krowa', 3, 'placki']
+
+myOwnForEach(bla, function (e) { console.log(e, this)}, {whatever: 'this is here'})
