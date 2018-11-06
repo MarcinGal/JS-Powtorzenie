d48527fa2f3a1b12d4afcd96a54430cb8608b93a
marcingal <marcin921125@gmail.com>
2018-11-06 09:56:47 +0100

    2exercise

--- script1.js
+++ script1.js
@@ -1,11 +1,33 @@
-function test () {
+// function test () {
 
+// }
+
+// const test2 = function () {
+
+// }
+
+// const test3 = function innerName () {}
+
+/*
+var log10Times = function innerName(i) {
+    i = i || 0;
+    console.log('log10Times')
+
+    if(i < 9) innerName(++i)
+}
+// log10Times()
+var log10TimesPrim = log10Times
+log10Times = null // always place const with function!
+
+log10TimesPrim()
+*/
+
+const tenNumbers = function numbers (i) {
+    i = (i === undefined) ? 10 : i
+    
+    console.log(i)
+
+    if(i > 0) numbers(--i) 
 }
 
-const test2 = function () {
-
-}
-
-const test3 = function innerName () {
-
-}
+tenNumbers()
